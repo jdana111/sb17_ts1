c2bcd3be07f4e88b97371033898789b47aa51c37
John Dana <john@danasd.com>
2023-05-16 16:37:48 -0600

    Play with basic typing

--- index.tsx
+++ index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
+type Salutation = {greeting: string, name: string}
+function greet({ greeting, name }: Salutation): string {
+  return `${greeting} ${name}`
+}
+
+greet({greeting: 'hello', name: 'Johnny'});
+
+let helloworld: string = 'Blah'
+
 const App = () => <div>Hi</div>
 
 const container = document.getElementById('app');
